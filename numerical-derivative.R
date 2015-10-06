399e82408c5baa9dc3d8c123b33a98cb1388321c
wiwh <wiwh404@gmail.com>
2015-10-06 20:08:11 +0200

    Merge branch 'master' into implement-jairo-comments
    Conflicts:
    	numerical-derivative.R

--- numerical-derivative.R
+++ numerical-derivative.R
@@ -1,1 +1,13 @@
-# function derivee numerique
+# Numerical derivative function
+
+numerical_derivative <- function(x0, f, h=1e-05){
+  # x0 is the initial point
+  # f is the function
+  # h is "step"
+  rightder <- (f(x0 + h) - f(x0)) / h
+  leftder <- (f(x0) - f(x0-h)) / h
+  out = NULL
+  out$rightder <- rightder
+  out$leftder <- leftder
+  out
+}
